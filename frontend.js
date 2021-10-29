bb9d2c9789fc8db973358aefbff1e191f753474b
AbmSourav <keramotul.islam@gmail.com>
2021-10-30 01:50:00 +0600

    static file with frontend style added

--- frontend.js
+++ frontend.js
@@ -3,9 +3,17 @@ export function App() {
 		`<html>
 			<head>
 				<title>Hello from Deno</title>
+				<link rel="stylesheet" href="style.css" />
 			</head>
 			<body>
 				<h1 style="text-align: center">Hello Universe...</h1>
+
+				<footer>
+					<p>
+						Serving from <strong>Deno Deploy</strong> by 
+						<a href="https://abmsourav.com">Abm Sourav</a>
+					</p>
+				</footer>
 			</body>
 		</html>`
 	);
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 import { listenAndServe } from "https://deno.land/std/http/server.ts";
 import { App } from "./frontend.js";
 
-function handler(req) {
+async function handler(req) {
+	const { pathname } = new URL(req.url);
+	if (pathname.startsWith("/style.css")) {
+		const style = await Deno.readFile("./style.css");
+		return new Response(style, {
+			headers: {
+				"content-type": "text/css",
+			}
+		});
+	}
+
 	return new Response(App(), {
 		headers: {
-			"content-type": "text/html",
+			"content-type": "text/html; charset=utf-8",
 		},
 	});
 }
